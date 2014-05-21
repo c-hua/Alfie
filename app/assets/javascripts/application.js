9b91d6879e0c81716c7f753d0fad0f6e37f22a39
Christie Hua <christiehua@gmail.com>
2014-05-20 23:07:13 -0700

    user views - jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,4 +35,21 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $('a[href="#box3"]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
+
 </script>
